ecbb840fcfdf40ac7a2cf37812411c424021b084
0xnewton <0xnewton@protonmail.com>
2022-04-13 02:43:05 -0400

    remove logging import

--- contracts/LootboxInstant.sol
+++ contracts/LootboxInstant.sol
@@ -4,7 +4,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
